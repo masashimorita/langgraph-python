40262c74c2f795f7f372f7670b2fa86b56024fda
masashimorita <masashi.morita.mm@gmail.com>
2025-04-29 08:13:30 +0900

    :+1: Implemented a reflection agent for making better tweet baed on input

---
+++ reflection_agent/main.py
@@ -0,0 +1,54 @@
+from typing import List, Sequence
+from dotenv import load_dotenv
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph import END, MessageGraph
+
+from chains import generate_chain, reflect_chain
+
+load_dotenv()
+
+REFLECT = "reflect"
+GENERATE = "generate"
+
+
+def generation_node(state: Sequence[BaseMessage]):
+  return generate_chain.invoke({"messages": state})
+
+
+def reflection_node(messages: Sequence[BaseMessage]) -> List[BaseMessage]:
+  res = reflect_chain.invoke({"messages": messages})
+  return [HumanMessage(content=res.content)]
+
+
+def should_continue(state: List[BaseMessage]):
+  if len(state) > 6:
+    return END
+  return REFLECT
+
+
+builder = MessageGraph()
+builder.add_node(GENERATE, generation_node)
+builder.add_node(REFLECT, reflection_node)
+builder.set_entry_point(GENERATE)
+builder.add_conditional_edges(GENERATE, should_continue)
+builder.add_edge(REFLECT, GENERATE)
+
+
+graph = builder.compile()
+# print(graph.get_graph().draw_mermaid())
+# print(graph.get_graph().print_ascii())
+
+
+if __name__ == "__main__":
+  print("Hello LangGraph!")
+
+  inputs = HumanMessage(content="""\
+    Make this twee better:
+    @LangChainAI
+    - newly Tool Calling feature is seriously underrated.
+    After a long wait, it's here - making the implementation of agents across different models with funcion calling - super easy.
+    Made a video covering their newest blog post.
+  """)
+
+  output = graph.invoke(inputs)
+  print(output)
