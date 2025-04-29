860772a05bd109582c4f047c897c916a4ecefd4c
masashimorita <masashi.morita.mm@gmail.com>
2025-04-29 18:15:55 +0900

    :sparkles: Implemented a responder chain

--- reflexion_agent/chains.py
+++ reflexion_agent/chains.py
@@ -1,18 +1,21 @@
 import datetime
 from dotenv import load_dotenv
 from langchain_core.output_parsers.openai_tools import (
-  JsonOutputToolParser,
+  JsonOutputToolsParser,
   PydanticToolsParser
 )
 from langchain_core.messages import HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
 
+from schemas import AnswerQuestion
 
 load_dotenv()
 
 
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+parser = JsonOutputToolsParser(return_id=True)
+parser_pydantic = PydanticToolsParser(tools=[AnswerQuestion])
 
 actor_prompt_template = ChatPromptTemplate.from_messages([
   ("system", """
@@ -28,3 +31,26 @@ actor_prompt_template = ChatPromptTemplate.from_messages([
 ]).partial(
   time=lambda: datetime.datetime.now().isoformat()
 )
+
+first_responder_prompt_template = actor_prompt_template.partial(
+  first_instruction="Provide a detailed ~250 word answer.",
+)
+
+first_responder = first_responder_prompt_template | llm.bind_tools(
+  tools=[AnswerQuestion], tool_choice="AnswerQuestion"
+)
+
+if __name__ == "__main__":
+  human_message = HumanMessage(content="""\
+    Write about AI-Powered SOC / autonomous soc problem domain,
+    list startups that do that and raised capital. 
+  """)
+
+  chain = (
+    first_responder_prompt_template
+    | llm.bind_tools(tools=[AnswerQuestion], tool_choice="AnswerQuestion")
+    | parser_pydantic
+  )
+
+  response = chain.invoke({"messages": [human_message]})
+  print(response)
