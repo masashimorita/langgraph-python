7a4e4f6aecd861a8790fd63ab104a347a46cf44e
masashimorita <masashi.morita.mm@gmail.com>
2025-05-03 11:55:19 +0900

    :+1: Add tests for retrieval_grader chain for agentic_rag

--- agentic_rag/graph/chains/tests/test_chains.py
+++ agentic_rag/graph/chains/tests/test_chains.py
@@ -1,2 +1,26 @@
-def test_foo() -> None:
-    assert 1 == 1
+from dotenv import load_dotenv
+from graph.chains.retrieval_grader import retrieval_grader, GradeDocuments
+from ingestion import retriever
+
+
+load_dotenv()
+
+
+def test_retrieval_grader_answer_yes() -> None:
+  question = "agent memory"
+  docs = retriever.invoke(question)
+  doc_text = docs[0].page_content
+
+  res: GradeDocuments = retrieval_grader.invoke({"question": question,"document": doc_text,})
+
+  assert res.binary_score == "yes"
+  
+
+def test_retrieval_grader_answer_no() -> None:
+  question = "agent memory"
+  docs = retriever.invoke(question)
+  doc_text = docs[0].page_content
+
+  res: GradeDocuments = retrieval_grader.invoke({"question": "how to make pizza","document": doc_text,})
+
+  assert res.binary_score == "no"
