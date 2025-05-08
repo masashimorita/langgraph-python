b8b5e703699ed803788574efe5f0a992da9eabad
masashimorita <masashi.morita.mm@gmail.com>
2025-05-09 06:17:45 +0900

    :+1: Define graph & nodes for async graph execution

---
+++ async_graph/async.py
@@ -0,0 +1,44 @@
+import operator
+import time
+from typing import Annotated, Any, TypedDict
+from dotenv import load_dotenv
+from langgraph.graph import StateGraph, END, START
+
+
+load_dotenv()
+
+
+class State(TypedDict):
+  aggregate: Annotated[list, operator.add]
+
+
+class ReturnNodeValue:
+  def __init__(self, node_secret: str):
+    self._value = node_secret
+  
+  def __call__(self, state: State) -> Any:
+    time.sleep(1)
+    print(f"Adding {self._value} to {state['aggregate']}")
+    return {"aggregate": [self._value]}
+
+
+builder = StateGraph(State)
+builder.add_node("a", ReturnNodeValue("I'm A"))
+builder.add_node("b", ReturnNodeValue("I'm B"))
+builder.add_node("c", ReturnNodeValue("I'm C"))
+builder.add_node("d", ReturnNodeValue("I'm D"))
+
+
+builder.add_edge(START, "a")
+builder.add_edge("a", "b")
+builder.add_edge("a", "c")
+builder.add_edge("b", "d")
+builder.add_edge("c", "d")
+builder.add_edge("d", END)
+
+
+app = builder.compile()
+
+if __name__ == "__main__":
+  print("Hello Async Graph");
+  app.invoke({"aggregate": []}, {"configurable": {"thread_id": "async_graph"}})
