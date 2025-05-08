4e1871c6a90f65631487e86f293d868acac3b792
masashimorita <masashi.morita.mm@gmail.com>
2025-05-09 06:22:30 +0900

    :+1: Add more nodes to async graph

--- async_graph/async.py
+++ async_graph/async.py
@@ -25,6 +25,7 @@ class ReturnNodeValue:
 builder = StateGraph(State)
 builder.add_node("a", ReturnNodeValue("I'm A"))
 builder.add_node("b", ReturnNodeValue("I'm B"))
+builder.add_node("b2", ReturnNodeValue("I'm B2"))
 builder.add_node("c", ReturnNodeValue("I'm C"))
 builder.add_node("d", ReturnNodeValue("I'm D"))
 
@@ -32,8 +33,8 @@ builder.add_node("d", ReturnNodeValue("I'm D"))
 builder.add_edge(START, "a")
 builder.add_edge("a", "b")
 builder.add_edge("a", "c")
-builder.add_edge("b", "d")
-builder.add_edge("c", "d")
+builder.add_edge("b", "b2")
+builder.add_edge(["b2", "c"], "d")
 builder.add_edge("d", END)
 
 
