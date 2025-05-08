278b9fa9e696be7a1cda7f6d38ae568c31779bfe
masashimorita <masashi.morita.mm@gmail.com>
2025-05-09 06:33:24 +0900

    :+1: Execute conditional branch for async execution

--- async_graph/async.py
+++ async_graph/async.py
@@ -1,6 +1,6 @@
 import operator
 import time
-from typing import Annotated, Any, TypedDict
+from typing import Annotated, Any, TypedDict, Sequence
 from dotenv import load_dotenv
 from langgraph.graph import StateGraph, END, START
 
@@ -10,6 +10,7 @@ load_dotenv()
 
 class State(TypedDict):
   aggregate: Annotated[list, operator.add]
+  which: str
 
 
 class ReturnNodeValue:
@@ -22,24 +23,32 @@ class ReturnNodeValue:
     return {"aggregate": [self._value]}
 
 
+def route_bc_or_cd(state: State) -> Sequence[str]:
+  if state["which"] == "cd":
+    return ["c", "d"]
+  return ["b", "c"]
+
+
+intermediates = ["b", "c", "d"]
+
+
 builder = StateGraph(State)
 builder.add_node("a", ReturnNodeValue("I'm A"))
 builder.add_node("b", ReturnNodeValue("I'm B"))
-builder.add_node("b2", ReturnNodeValue("I'm B2"))
 builder.add_node("c", ReturnNodeValue("I'm C"))
 builder.add_node("d", ReturnNodeValue("I'm D"))
+builder.add_node("e", ReturnNodeValue("I'm E"))
 
 
 builder.add_edge(START, "a")
-builder.add_edge("a", "b")
-builder.add_edge("a", "c")
-builder.add_edge("b", "b2")
-builder.add_edge(["b2", "c"], "d")
-builder.add_edge("d", END)
-
-
+builder.add_conditional_edges("a", route_bc_or_cd, intermediates)
+for node in intermediates:
+  builder.add_edge(node, "e")
+builder.add_edge("e", END)
 app = builder.compile()
+
+print(app.get_graph().draw_ascii())
 
 if __name__ == "__main__":
   print("Hello Async Graph");
-  app.invoke({"aggregate": []}, {"configurable": {"thread_id": "async_graph"}})
+  app.invoke({"aggregate": [], "which": "cd"}, {"configurable": {"thread_id": "async_graph"}})
